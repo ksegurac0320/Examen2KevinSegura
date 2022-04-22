60f86eb806aa37e82bc3ee276ef5e559aff732d4
ksegurac0320 <ksegurac0320@gmail.com>
2022-04-22 16:52:16 -0500

    Cambio entity

--- Entity/DBEntity.cs
+++ Entity/DBEntity.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace Entity
 {
     public class DBEntity
-    {   //clase oara mappear los errores que sql nos mande
+    {   //clase oara mappear los errores que sql nos envie
         public int CodeError { get; set; }
         public string MsgError { get; set; }
     }
